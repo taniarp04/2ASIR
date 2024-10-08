4e536a25f94d21f3e94690f37cc7f98d4f5618c2
taniarp04 <Taniareciopolo2004@gmail.com>
2024-10-08 09:47:14 +0200

    Add files via upload
    Mayor de array

---
+++ ejercicio7.php
@@ -0,0 +1,10 @@
+<?php
+// Declarar un array con 7 valores numéricos
+$numeros = [15, 23, 8, 42, 16, 4, 31];
+
+// Encontrar el mayor número usando la función max()
+$mayor = max($numeros);
+
+// Mostrar el número mayor
+echo "El número mayor es: " . $mayor;
+?>
